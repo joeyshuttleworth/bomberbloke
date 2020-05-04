6a0c2c57eb2b48588033182e8211f2098c6ffd9d
davenewham <david@newh.am>
2020-05-04 14:30:02 +0200

    delete uneeded files and add references to new locations

---
+++ build/CMakeFiles/fmt.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fmt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/fmt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
