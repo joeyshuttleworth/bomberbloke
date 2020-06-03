57dfd24e2dbfe2ae67fb71c055d908f899a3478c
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2020-06-03 19:48:34 +0100

    Cereal inheritance working with QueryEvent.

---
+++ engine/event/JoinEvent.hpp
@@ -0,0 +1,36 @@
+/*
+  Event that describes how an actor has moved.
+  This is used by both the server and client to
+  update the position of actors. An up to date
+  list of actors is required for this to work.
+  Such a list is sent using the SyncEvent class
+  and update using the CreateEvent and RemoveEvent
+  classes
+*/
+
+#ifndef JOINEVENT_HPP
+
+#define JOINEVENT_HPP
+#include "AbstractEvent.hpp"
+#include <cereal/types/vector.hpp>
+
+class MoveEvent : AbstractEvent{
+private:
+  player mPlayer;
+public:
+
+  MoveEvent(player* Player){
+    mPlayer = *Player;
+    return;
+  };
+
+  template<class Archive>
+  /*Used by cereal to serialize the event for it to be sent/received*/
+  void serialize(Archive &archive){
+    archive(cereal::make_nvp("Nickname", mPlayer.nickname))
+  }
+
+};
+
+#endif
+ 
