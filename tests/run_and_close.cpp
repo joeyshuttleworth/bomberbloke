073065e0169b55dd35a05d8452ceb7006b4def1c
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2020-06-08 17:39:27 +0100

    Added a test (finally).

---
+++ tests/run_and_close.cpp
@@ -0,0 +1,44 @@
+#include "bomberbloke.h"
+#include "cereal/archives/json.hpp"
+#include <SDL2/SDL.h>
+#include "bloke.hpp"
+#include "bomb.hpp"
+
+const std::vector<command_binding> _default_bindings;
+
+int main (){
+  _draw=false;
+
+  SDL_Init(SDL_INIT_EVERYTHING);
+
+  init_engine();
+  _local_player_list.push_back(localPlayer(std::string("big_beef")));
+  cereal::JSONOutputArchive oArchive(std::cout);
+
+  std::shared_ptr<bloke> b1(new bloke(5,5));
+  _level.mActors.push_back(b1);
+  _local_player_list.back().init(b1);
+  oArchive(b1,  _level);
+
+  _halt = true;
+  client_loop();
+
+  SDL_Delay(2000);
+  SDL_Quit();
+
+  return 0;
+}
+
+
+void new_game(std::string){
+  return;
+}
+
+
+
+
+
+
+
+
+
