ac2a856fa5c045eda37500ad469ec5ad82f76997
tylerkf <tylerf98@live.co.uk>
2020-07-19 07:15:12 +0100

    Added pause and fade out

--- engine/sound/Sound.hpp
+++ engine/sound/Sound.hpp
@@ -53,11 +53,43 @@ public:
         mMixChunk = soundFile;
     }
     
+    /**
+     * Pauses sound
+     */
+    void pause() {
+        if (channel >= 0)
+            Mix_Pause(channel);
+    }
     
     /**
+     * Resumes sound
      */
+    void resume() {
+        if (channel >= 0)
+            Mix_Resume(channel);
+    }
     
+    /**
+     * Stops sound
+     */
+    void stop(int waitMs=0) {
+        if (channel >= 0) {
+            if (waitMs > 0) {
+                Mix_ExpireChannel(channel, waitMs);
+            } else {
+                Mix_HaltChannel(channel);
+            }
+        }
+    }
     
+    /**
+     * Begins fade out effect at time of call. ms is the number of milliseconds
+     * that the fade-out effect should take to go to silence
+     */
+    void fadeOut(int ms) {
+        if (channel >= 0)
+            Mix_FadeOutChannel(channel, ms);
+    }
 };
 
 #endif
