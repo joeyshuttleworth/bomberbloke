77b5ef46987fa0a74d3cf239be1ac782d1e5a312
tylerkf <tylerf98@live.co.uk>
2020-08-19 03:47:49 +0100

    Cleaned up camera.hpp

--- engine/camera/Camera.cpp
+++ engine/camera/Camera.cpp
@@ -2,7 +2,32 @@
 #include "Camera.hpp"
 
 void Camera::rumble(double amplitude, double timeout){
-  mRumbleTimeout = timeout;
-  mRumbleAmplitude = amplitude;
-  return;
+    mRumbleTimeout = timeout;
+    mRumbleAmplitude = amplitude;
+    return;
+}
+
+void Camera::draw() {
+    update();
+    mScreenRectangle.x = mRumbleOffset[0];
+    mScreenRectangle.y = mRumbleOffset[1];
+    SDL_SetRenderTarget(_renderer, mpFrameBuffer);
+    mpScene->draw(this);
+    SDL_SetRenderTarget(_renderer, nullptr);
+    SDL_RenderCopy(_renderer, mpFrameBuffer, nullptr, &mScreenRectangle);
+    
+    return;
+}
+
+void Camera::update() {
+    /*  Rumble effect */
+    if(mRumbleTimeout > 0){
+        mRumbleTimeout--;
+        mRumbleOffset[0] = mRumbleAmplitude*mWidth*std::sin((double)mRumbleTimeout/2);
+        mRumbleOffset[1] = mRumbleAmplitude*mHeight*std::cos((double)mRumbleTimeout/5);
+    } else {
+        mRumbleOffset[0] = 0;
+        mRumbleOffset[1] = 0;
+    }
+    return;
 }
--- engine/camera/Camera.hpp
+++ engine/camera/Camera.hpp
@@ -65,33 +65,12 @@ public:
     return mpScene;
   }
 
-  void draw(){
-    update();
-    mScreenRectangle.x = mRumbleOffset[0];
-    mScreenRectangle.y = mRumbleOffset[1];
-    SDL_SetRenderTarget(_renderer, mpFrameBuffer);
-    mpScene->draw(this);
-    SDL_SetRenderTarget(_renderer, nullptr);
-    SDL_RenderCopy(_renderer, mpFrameBuffer, nullptr, &mScreenRectangle);
-    return;
-  }
+  void draw();
 
    /* This could be used to make (for example) a FollowCamera class which
    *  inherits from this class
    */
-  virtual void update(){
-    /*  Rumble effect */
-    if(mRumbleTimeout > 0){
-      mRumbleTimeout--;
-      mRumbleOffset[0] = mRumbleAmplitude*mWidth*std::sin((double)mRumbleTimeout/2);
-      mRumbleOffset[1] = mRumbleAmplitude*mHeight*std::cos((double)mRumbleTimeout/5);
-    }
-    else{
-      mRumbleOffset[0] = 0;
-      mRumbleOffset[1] = 0;
-    }
-    return;
-  };
+  virtual void update();
 
   void rumble(double amplitude = 0.02, double timeout = 30);
 protected:
