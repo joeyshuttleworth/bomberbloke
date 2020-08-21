d6188cab32db8103448b235c713fea380071553f
Tyler Farghly <tylerf98@live.co.uk>
2020-08-22 00:44:31 +0100

    Added text rendering (#20)
    * Added SDL2 and text folder to cmake
    
    * Added TextManager
    
    * Added Text
    
    * Add destructors for TextManager and SoundManager
    
    * Test code for text
    
    * Fixed pointer bug
    
    * Added textManager to engine
    
    * Added test font
    
    * Refactored text code
    
    * Fixed bug
    
    * Fixed bug
    
    * Added SDL_TTF to travis
    
    * Include Memory for Shared Pointers
    
    Co-authored-by: David Newham <davidpaulnewham@gmail.com>

---
+++ engine/text/TextManager.hpp
@@ -0,0 +1,50 @@
+#ifndef TEXTMANAGER_HPP
+#define TEXTMANAGER_HPP
+
+#include <SDL2/SDL_ttf.h>
+#include <memory>
+#include <string>
+#include <map>
+
+class Text;
+
+/**
+ * Loads and stores fonts and creates Text objects.
+ */
+class TextManager {
+public:
+    /**
+     * Constructor for TextManager.
+     */
+    TextManager();
+    
+    /**
+     * Destructor for TextManager.
+     */
+    ~TextManager();
+    
+    /**
+     * Loads .ttf font file.
+     *
+     * @param path      String containing path to .ttf file.
+     * @param id        String that will be used to refer to the font when
+     *                  creating Text objects.
+     * @param ptSize    Point-size of font that is loaded.
+     */
+    void loadFontFromPath(std::string path, std::string id, int ptSize=16);
+    
+    /**
+     * Creates a text object given a specified font.
+     *
+     * Font must first be loaded using loadFontFromPath.
+     * 
+     * @param fontId    ID corresponding to font (see loadFontFromPath).
+     */
+    std::shared_ptr<Text> createText(std::string fontId);
+
+private:
+    // Map from font ID (see loadFontFromPath) to SDL font object.
+    std::map<std::string, TTF_Font*> mFontBank;
+};
+
+#endif
