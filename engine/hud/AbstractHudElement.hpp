8dff82d416d5e55f9c541342d6f4d4527081074e
tylerkf <tylerf98@live.co.uk>
2020-08-22 16:07:43 +0100

    Refactored AbstractHudElement

--- engine/hud/AbstractHudElement.hpp
+++ engine/hud/AbstractHudElement.hpp
@@ -1,13 +1,10 @@
 #ifndef ABSTRACTHUDElEMENT_HPP
 #define ABSTRACTHUDElEMENT_HPP
 
-#include <SDL2/SDL.h>
-
 #include "engine.hpp"
 
 class Camera;
 extern SDL_Renderer *_renderer;
-SDL_Texture* get_sprite(std::string);
 
 enum AlignFlag {
     ALIGN_LEFT,
@@ -17,6 +14,9 @@ enum AlignFlag {
     ALIGN_TOP
 };
 
+/**
+ * Abstract class for HUD elements that are stored, updated and drawn in Scene.
+ */
 class AbstractHudElement {
 public:
     /**
@@ -25,7 +25,7 @@ public:
     bool mIsInteractive = false;
 
      /**
-      * @brief Base class constructor for HUD
+      * Base class constructor for HUD.
       *
       * If xAlignFlag is set to ALIGN_LEFT the bounding box will be positioned with
       * its left side xPos pixels away from the left side of the window. If
@@ -35,80 +35,79 @@ public:
       * similarly with ALIGN_TOP corresponding to alignment relative to the top of the
       * window and ALIGN_BOTTOM corresponding to the bottom.
       *
-      * @param xPos - the position of the hud element in pixels
-      * @param yPos - the position of the hud element in pixels
-      * @param xDim - give the dimensions of the bounding box of the element in px
-      * @param yDim - give the dimensions of the bounding box of the element in px
-      * @param xAlignFlag - AlignFlag determine the alignment of the bounding box.
-      * @param yAlignFlag  - determine the alignment of the bounding box.
+      * @param xPos          The position of the hud element in px.
+      * @param yPos          The position of the hud element in px.
+      * @param xDim          The dimensions of the bounding box of the element in px.
+      * @param yDim          The dimensions of the bounding box of the element in px.
+      * @param xAlignFlag    Determines the alignment of the bounding box.
+      * @param yAlignFlag    DetermineS the alignment of the bounding box.
       */
     AbstractHudElement(int xPos, int yPos, int xDim, int yDim,
-                       AlignFlag xAlignFlag=ALIGN_LEFT, AlignFlag yAlignFlag = ALIGN_BOTTOM) {
-        // Actual position is set in updatePosition
-        mRelativePosition[0] = xPos;
-        mRelativePosition[1] = yPos;
-        
+            AlignFlag xAlignFlag=ALIGN_LEFT, AlignFlag yAlignFlag=ALIGN_TOP);
+    
+    /**
+     * Sets the (relative) screen position of the bounding box.
+     *
+     * The position corresponds to the top-left corner of the box. See the
+     * constructor for information on how this property interacts with
+     * alignment.
+     *
+     * @param xPos  X-coordinate of the screen position (pixels).
+     * @param yPos  Y-coordinate of the screen position (pixels).
+     */
+    void setPosition(int xPos, int yPos) {
+        mPosition[0] = xPos;
+        mPosition[1] = yPos;
+        mPropertiesUpdated = true;
+    }
+    
+    /**
+     * Sets the dimensions of the bounding box.
+     *
+     * Sets the size (in pixels) of the bounding box.
+     *
+     * @param xDim  Number of pixels wide.
+     * @param yDim  Number of pixels high.
+     */
+    void setDimensions(int xDim, int yDim) {
         mDimensions[0] = xDim;
         mDimensions[1] = yDim;
-        
-        mAlignFlags[0] = xAlignFlag;
-        mAlignFlags[1] = yAlignFlag;
+        mPropertiesUpdated = true;
+    }
+    
+    /**
+     * Sets the alignment of the bounding box. (see constructor)
+     *
+     * @param xAlign    Horizontal alignment of the box. Accepts ALIGN_LEFT,
+     *                  ALIGN_RIGHT and ALIGN_CENTER (default).
+     * @param yAlign    Horizontal alignment of the box. Accepts ALIGN_TOP,
+     *                  ALIGN_BOTTOM and ALIGN_CENTER (default).
+     */
+    void setTextAlignment(AlignFlag xAlign, AlignFlag yAlign=ALIGN_TOP) {
+        mAlignFlags[0] = xAlign;
+        mAlignFlags[1] = yAlign;
     }
 
     /**
      * Draws a grey box corresponding to the bounding box.
+     *
      * Called by scene on every frame. updatePosition must be called once at
      * some point before it is drawn.
+     * 
+     * @param camera    Current Camera object.
      */
-    virtual void draw(Camera* camera) {
-        SDL_Rect dstRect;
-        dstRect.x = mPosition[0];
-        dstRect.y = mPosition[1];
-        dstRect.w = mDimensions[0];
-        dstRect.h = mDimensions[1];
-        
-        SDL_SetRenderDrawColor(_renderer, 0xa0, 0xa0, 0xa0, 0xff);
-        SDL_RenderFillRect(_renderer, &dstRect);
-        
-        return;
-    }
+    virtual void draw(Camera* camera);
     
     /**
+     * Updates the position of the HUD element.
+     *
      * Converts the relative position, dimension and alignment flags into the
      * actual position of the bounding box from its top left corner. This must
      * be called at least once before draw is called.
+     * 
+     * @param camera    Current Camera object.
      */
-    void updatePosition(Camera* camera) {
-        std::array<int, 2> screenDimensions = camera->getScreenDimensions();
-        
-        switch(mAlignFlags[0]) {
-            case ALIGN_CENTER:
-                // Centred positioning
-                mPosition[0] = (screenDimensions[0] - mDimensions[0]) / 2;
-                break;
-            case ALIGN_RIGHT:
-                // Right-aligned positioning
-                mPosition[0] = screenDimensions[0] - mDimensions[0] - mRelativePosition[0];
-                break;
-            default:
-                // Left-aligned positioning
-                mPosition[0] = mRelativePosition[0];
-        }
-        
-        switch(mAlignFlags[1]) {
-            case ALIGN_CENTER:
-                // Centred positioning
-                mPosition[1] = (screenDimensions[1] - mDimensions[1]) / 2;
-                break;
-            case ALIGN_RIGHT:
-                // Right-aligned positioning
-                mPosition[1] = screenDimensions[1] - mDimensions[1] - mRelativePosition[1];
-                break;
-            default:
-                // Left-aligned positioning
-                mPosition[1] = mRelativePosition[1];
-        }
-    }
+    virtual void updatePosition(Camera* camera);
 
     /**
      * Update method to be called on every tick.
@@ -117,31 +116,29 @@ public:
     
     /**
      * Called by scene whenever the engine detects user input.
+     *
+     * @param event Input event that is handled by HUD element.
      */
     virtual void onInput(SDL_Event *event) {};
     
 protected:
-    /**
-     * Pixel-position of the top left corner of the bounding box.
-     */
+    // Pixel-position of the top left corner of the bounding box.
     int mPosition[2];
     
-    /**
-     * Pixel-dimensions of the bounding box containing the element.
-     */
+    // Pixel-dimensions of the bounding box containing the element.
     int mDimensions[2];
 
-    /**
-     * Relative positional values (see xPos and yPos in the constructor) that
-     * are converted to actual positional values in updatePosition based on the
-     * parameters of the current camera and the alignment flags.
-     */
+    // Relative positional values (see xPos and yPos in the constructor) that
+    // are converted to actual positional values in updatePosition based on the
+    // parameters of the current camera and the alignment flags.
     int mRelativePosition[2];
     
-    /**
-     * x and y alignment flags (see constructor).
-     */
+    // x and y alignment flags (see constructor).
     int mAlignFlags[2];
+    
+    // Boolean value which is set to true whenever a property is changed that
+    // may effect the render. Set back to false when draw is called.
+    bool mPropertiesUpdated;
 };
 
 #endif
