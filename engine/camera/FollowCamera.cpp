dbf50b4a645e20ec55db9b198bbba6230e9ccec2
tylerkf <tylerf98@live.co.uk>
2020-09-02 03:24:07 +0100

    Added FollowCamera

---
+++ engine/camera/FollowCamera.cpp
@@ -0,0 +1,68 @@
+#include "FollowCamera.hpp"
+#include "actor.hpp"
+
+#include <math.h>
+
+void FollowCamera::update() {
+    Camera::update();
+
+    std::shared_ptr<actor> subjectPtr = mSubject.lock();
+    if (subjectPtr) {
+        double distance = std::pow(std::pow(subjectPtr->mPosition[0] - mPosition[0], 2)
+            + std::pow(subjectPtr->mPosition[1] - mPosition[1], 2), 0.5);
+
+        if (distance > CAMERA_MAX_DISTANCE) {
+            // If the camera distance exceeds the maximum move straight to actor
+            mVelocity[0] = 0;
+            mVelocity[1] = 0;
+            mPosition[0] = subjectPtr->mPosition[0];
+            mPosition[1] = subjectPtr->mPosition[1];
+        } else if (distance == 0) {
+            // Set velocity to zero if on actor
+            mVelocity[0] = 0;
+            mVelocity[1] = 0;
+        } else {
+            double speed = std::pow(std::pow(mVelocity[0], 2) + std::pow(mVelocity[1], 2), 0.5);
+
+            // Accelerate towards bloke
+            speed += CAMERA_ACCELERATION / TICK_RATE;
+
+            if (speed > CAMERA_MAX_SPEED) {
+                // Speed has exceeded maximum
+                speed = CAMERA_MAX_SPEED;
+            }
+
+            if (std::pow(speed, 2) > 2 * distance * CAMERA_ACCELERATION) {
+                // Decelerate on to actor
+                speed = std::pow(2 * distance * CAMERA_ACCELERATION, 0.5);
+            }
+
+            if (distance < speed / TICK_RATE) {
+                // Close enough - move onto subject
+                mVelocity[0] = 0;
+                mVelocity[1] = 0;
+                mPosition[0] = subjectPtr->mPosition[0];
+                mPosition[1] = subjectPtr->mPosition[1];
+            } else {
+                // Point velocity in direction of subject
+                mVelocity[0] = (subjectPtr->mPosition[0] - mPosition[0]) / distance * speed;
+                mVelocity[1] = (subjectPtr->mPosition[1] - mPosition[1]) / distance * speed;
+                mPosition[0] += mVelocity[0] / TICK_RATE;
+                mPosition[1] += mVelocity[1] / TICK_RATE;
+            }
+        }
+    } else if (mVelocity[0] != 0 || mVelocity[1] != 0) {
+        double speed = std::pow(std::pow(mVelocity[0], 2) + std::pow(mVelocity[1], 2), 0.5);
+
+        // Camera isn't attached to any subject - decelerate to 0 velocity
+        if (CAMERA_ACCELERATION / TICK_RATE < speed) {
+            mVelocity[0] -= mVelocity[0] / speed * CAMERA_ACCELERATION / TICK_RATE;
+            mVelocity[1] -= mVelocity[1] / speed * CAMERA_ACCELERATION / TICK_RATE;
+            mPosition[0] += mVelocity[0] / TICK_RATE;
+            mPosition[1] += mVelocity[1] / TICK_RATE;
+        } else {
+            mVelocity[0] = 0;
+            mVelocity[1] = 0;
+        }
+    }
+}
---
+++ engine/camera/FollowCamera.hpp
@@ -0,0 +1,31 @@
+#ifndef FOLLOWCAMERA_HPP
+#define FOLLOWCAMERA_HPP
+
+#include "Camera.hpp"
+
+#include <array>
+#include <memory>
+
+class actor;
+
+const double CAMERA_ACCELERATION = 50;
+const double CAMERA_MAX_SPEED = 50;
+const double CAMERA_MAX_DISTANCE = 10;
+
+class FollowCamera: public Camera {
+public:
+    // Subject that is followed by the camera
+    std::weak_ptr<actor> mSubject;
+
+    using Camera::Camera;
+
+    /**
+     * Updates camera position and velocity.
+     */
+    void update();
+protected:
+    // Velocity of camera in scene units.
+    std::array<double, 2> mVelocity = {{ 0, 0 }};
+};
+
+#endif
