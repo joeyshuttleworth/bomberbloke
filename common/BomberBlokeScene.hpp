9f105667264603abc19aed2ac0cef531fff8ad49
tylerkf <tylerf98@live.co.uk>
2020-09-06 22:05:28 +0100

    Added comments to methods

--- common/BomberBlokeScene.hpp
+++ common/BomberBlokeScene.hpp
@@ -12,8 +12,10 @@ protected:
   std::weak_ptr<AbstractHudElement> mPowerIcons[5];
   std::weak_ptr<AbstractHudElement> mSpeedIcons[5];
 
+  // Texture containing bakground tiles.
   SDL_Texture *mBackgroundTexture;
 
+  // Camera that follows local player.
   std::shared_ptr<FollowCamera> blokeCamera;
 
 public:
@@ -21,11 +23,10 @@ public:
     BomberBlokeScene(int size_x = 10, int size_y = 10);
     ~BomberBlokeScene(){}
 
-
     /**
-     * Update function
+     * Update function.
      */
-    void update();
+    void update() override;
 
    /**
      * Every second, check how many players are alive.
@@ -33,11 +34,20 @@ public:
      * if one player remains, this player wins the match.
      *
      */
-    void logicUpdate();
+    void logicUpdate() override;
 
-    void draw();
+    /**
+     * Draw function.
+     */
+    void draw() override;
 
-    void onInput(SDL_Event *event);
+    /**
+     * Handles input.
+     *
+     * Primarily for interactive HUD elements.
+     */
+    void onInput(SDL_Event *event) override;
+
 
     template<class Archive>
     void serialize(Archive &archive){
