af15381842c24c5df13576fb16f9d1c1857e8175
joeyshuttleworth <pmyjgs@nottingham.ac.uk>
2020-09-12 13:17:56 +0100

    Add sprites/animations for pickup objects (#50)
    * Added sprites for pickups. Changed acceleration constants
    
    * Added animation for pickup objects.

---
+++ common/PowerPickup.hpp
@@ -0,0 +1,31 @@
+#ifndef POWERPICKUP_HPP
+#define POWERPICKUP_HPP
+#include "AbstractPickup.hpp"
+#include "staticSprite.hpp"
+#include "bomberbloke_actors.hpp"
+#include "PowerPickup.hpp"
+#include "PickupAnimation.hpp"
+
+class PowerPickup : public AbstractPickup{
+public:
+  int getType() const{return PICKUP_POWER;}
+
+  /* Need a default constructor for cereal */
+
+  PowerPickup(double xpos = 0, double ypos = 0) : AbstractPickup(xpos, ypos){
+    /*TODO: draw asset*/
+    mpSpriteHandler = std::make_shared<PickupAnimation>(mPosition[0], mPosition[1], mDimmension[0], mDimmension[1], "flames.png");
+    return;
+  }
+
+  void pickup(std::shared_ptr<bloke> b);
+
+  template<class Archive>
+  void serialize(Archive &archive){
+    archive(cereal::base_class<actor>(this));
+    return;
+  }
+};
+
+CEREAL_REGISTER_TYPE(PowerPickup)
+#endif
