97cd216ce6c2f61ee0af37863c2ee3865b27b95c
tylerkf <tylerf98@live.co.uk>
2020-09-18 02:14:32 +0100

    Added cursor to input field

--- engine/hud/InputField.cpp
+++ engine/hud/InputField.cpp
@@ -19,44 +19,75 @@ InputField::InputField(std::shared_ptr<Text> text, int xPos, int yPos,
   mDefaultText = text->getText();
 }
 
-
-
 void InputField::onInput(SDL_Event *event) {
+  if (event->type == SDL_MOUSEBUTTONUP && mHasFocus == true) {
+    // If mouse button up, reset mHasFocus before checking
+    // If button up is on the input field onClick will be called
+    mHasFocus = false;
+    mText->setCursorVisible(false);
+    if (mTextInput == "") {
+      mText->setText(mDefaultText);
+      mText->setTextColour(mDefaultColour);
+    }
+  }
   ClickableHudElement::onInput(event);
 
   if (mHasFocus) {
     if (event->type == SDL_KEYDOWN) {
-      if (event->key.keysym.sym == SDLK_BACKSPACE && textInput.length() > 0) {
-        mPropertiesUpdated = true;
-        textInput.pop_back();
+      if (event->key.keysym.sym == SDLK_BACKSPACE && mCursorIndex > 0) {
+        // Handle backspace
+        mTextInput = mTextInput.substr(0, mCursorIndex - 1) + mTextInput.substr(mCursorIndex);
+        mCursorIndex--;
+        mText->setText(mTextInput);
+        mText->setCursorIndex(mCursorIndex);
       } else if (event->key.keysym.sym == SDLK_v && SDL_GetModState() & KMOD_CTRL) {
-        textInput = SDL_GetClipboardText();
+        // Handle clipboard-paste
+        std::string pasteText = SDL_GetClipboardText();
+        mTextInput = mTextInput.substr(0, mCursorIndex) + pasteText + mTextInput.substr(mCursorIndex);
+        mCursorIndex += pasteText.length();
+        mText->setText(mTextInput);
+        mText->setCursorIndex(mCursorIndex);
+      } else if (event->key.keysym.sym == SDLK_LEFT && mCursorIndex > 0) {
+        // Handle left button
+        mCursorIndex--;
+        mText->setCursorIndex(mCursorIndex);
+      } else if (event->key.keysym.sym == SDLK_RIGHT && mCursorIndex < mTextInput.length()) {
+        // Handle right button
+        mCursorIndex++;
+        mText->setCursorIndex(mCursorIndex);
       }
     } else if (event->type == SDL_TEXTINPUT) {
-      mPropertiesUpdated = true;
+      // Handle generic text input
       if (!(SDL_GetModState() & KMOD_CTRL && (event->text.text[0] == 'v' || event->text.text[0] == 'V'))) {
-        textInput += event->text.text;
-        log_message(DEBUG, textInput.c_str());
+        mTextInput = mTextInput.substr(0, mCursorIndex) + event->text.text + mTextInput.substr(mCursorIndex);
+        mCursorIndex++;
+        mText->setText(mTextInput);
+        mText->setCursorIndex(mCursorIndex);
       }
     }
   }
-
 }
 
+void InputField::onClick(int x, int y) {
+  mHasFocus = true;
+  mText->setText(mTextInput);
+  mText->setTextColour(mInputColour);
 
-void InputField::draw(Camera *camera) {
-  if (mPropertiesUpdated) {
-    // Set colour and text offset according to mIsClicked and mIsMouseOver
-    if (!textInput.empty()) {
-      mText->setText(textInput);
-    }
-    if (mIsClicked) {
-      //Enter TextBox Input
-      mText->setTextColour(mOnClickColour);
-      mText->setTextOffset(mOnClickOffset[0], mOnClickOffset[1]);
+  // Set cursor position
+  if (mTextInput != "") {
+    mCursorIndex = mText->getCursorIndex(x);
+  } else {
+    mCursorIndex = 0;
+  }
+}
+
+void InputField::update() {
+  // Flashing cursor animation
+  if (mHasFocus && _tick % CURSOR_UPDATE_TICKS == 0) {
+    if ((_tick / CURSOR_UPDATE_TICKS) % 3 == 0) {
+      mText->setCursorVisible(false);
+    } else {
+      mText->setCursorVisible(true);
     }
   }
-
-  // Call draw function inherited fromm TextHUDElement
-  TextHudElement::draw(camera);
 }
