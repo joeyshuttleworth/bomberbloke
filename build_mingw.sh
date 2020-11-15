8f36918476f574c9001f91ae828887aa5868e3f7
Joseph Shuttleworth <pmyjgs@nottingham.ac.uk>
2020-11-15 11:57:47 +0000

    Add licenses for various dependencies

--- build_mingw.sh
+++ build_mingw.sh
@@ -3,6 +3,8 @@
 mkdir mingw_build
 cd mingw_build
 
-cmake -DCURL_STATICLIB=1 -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DCMAKE_SYSTEM_NAME=Generic -D_WIN32=TRUE -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc-posix -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++-posix -DCMAKE_RC_COMPILER="$(which x86_64-w64-mingw32-windres)" -DDLLTOOL="$(which x86_64-w64-mingw32-dlltool)" -DCMAKE_C_COMPILER_WORKS=1 -DCMAKE_CXX_COMPILER_WORKS=1 .. 
+cmake -DBUILD_SHARED_LIBS=OFF -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DCMAKE_SYSTEM_NAME=Generic -D_WIN32=TRUE -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc-posix -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++-posix -DCMAKE_RC_COMPILER="$(which x86_64-w64-mingw32-windres)" -DDLLTOOL="$(which x86_64-w64-mingw32-dlltool)" -DCMAKE_C_COMPILER_WORKS=1 -DCMAKE_CXX_COMPILER_WORKS=1 ..
+
+cp ../windows_libraries/dlls/* .
 
 make
