fa5ecef539bdeacb7aef1909fb11c1b435a555fb
harveynw <harveywilliams@live.com>
2023-06-07 01:49:47 +0100

    address default

--- engine/hud/JoinMenuHudGroup.cpp
+++ engine/hud/JoinMenuHudGroup.cpp
@@ -197,6 +197,8 @@ JoinMenuHudGroup::update()
     // Get address in text field
     std::shared_ptr<InputField> addressField = mAddressField.lock();
     std::string address = addressField->mText->getText();
+    if(address.size() == 0)
+      address = "localhost"
 
     // Set nickname
     handle_system_command({ "nickname", nickname });
