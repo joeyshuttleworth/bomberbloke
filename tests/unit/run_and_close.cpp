fdfb51070ae5e24f11650c064e4cc7bb9040db8f
David Newham <davidpaulnewham@gmail.com>
2025-04-06 00:24:26 +0200

    add first catch2 test

---
+++ tests/unit/run_and_close.cpp
@@ -0,0 +1,40 @@
+#include <catch2/catch_test_macros.hpp>
+#include "BomberBlokeScene.hpp"
+#include "bloke.hpp"
+#include "bomb.hpp"
+#include "bomberbloke.h"
+#include <SDL2/SDL.h>
+
+extern int _log_message_level;
+
+TEST_CASE("Can make a game scene", "[engine]") {
+  _draw = false;
+  _log_message_level = CRITICAL;
+
+  REQUIRE(SDL_Init(SDL_INIT_EVERYTHING) == 0);
+  init_engine(false);
+
+  const std::string username = "big_beef";
+  _local_player_list.push_back(LocalPlayer(username));
+
+  _pScene = std::make_shared<BomberBlokeScene>(10, 10);
+  REQUIRE(_pScene != nullptr);
+  REQUIRE(_pScene->mDimmension[0] == 10);
+  REQUIRE(_pScene->mDimmension[1] == 10);
+
+  _halt = true;
+  client_loop();
+
+  SDL_Quit();
+}
+
+void
+gameUpdate()
+{
+  return;
+}
+
+void new_game(std::string)
+{
+  return;
+}
